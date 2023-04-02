7fdd4fd842bbb90d6e2c37dfc60259ebb0c70b08
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-02 17:43:03 +0300

    Initial commit

---
+++ src/main/java/com/javacodeset/JavaCodeSetApplication.java
@@ -0,0 +1,14 @@
+package com.javacodeset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class JavaCodeSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeSetApplication.class, args);
+    }
+}
