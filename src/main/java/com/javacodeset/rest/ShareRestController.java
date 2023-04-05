46cb399ea71ea0cf99e5515668814b70157adf85
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-05 21:25:07 +0300

    ShareRestController added

---
+++ src/main/java/com/javacodeset/rest/ShareRestController.java
@@ -0,0 +1,35 @@
+package com.javacodeset.rest;
+
+import com.javacodeset.dto.ShareDto;
+import com.javacodeset.service.api.ShareService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/shares")
+@RequiredArgsConstructor
+public class ShareRestController {
+
+    private final ShareService shareService;
+    private final ModelMapper modelMapper;
+
+    @PostMapping("/create")
+    public ShareDto createShare(@RequestBody ShareDto shareDto) {
+        return modelMapper.map(shareService.create(shareDto), ShareDto.class);
+    }
+
+    @GetMapping("/get/{shareId}")
+    public ShareDto getShareById(@PathVariable UUID shareId) {
+        return modelMapper.map(shareService.get(shareId), ShareDto.class);
+    }
+
+    @GetMapping("/get-all/to-user/{userId}")
+    public List<ShareDto> getAllSharesToUserId(@PathVariable UUID userId) {
+        return shareService.getAllSharesToUserId(userId).stream()
+                .map(shareEntity -> modelMapper.map(shareEntity, ShareDto.class)).toList();
+    }
+}
