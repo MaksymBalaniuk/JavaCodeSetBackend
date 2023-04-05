78b52b4044e8a8634b6ec318d8b754be9ac6f7ad
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-05 20:45:34 +0300

    AuthenticationRestController added

---
+++ src/main/java/com/javacodeset/rest/AuthenticationRestController.java
@@ -0,0 +1,30 @@
+package com.javacodeset.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.javacodeset.dto.auth.AuthenticationRequestDto;
+import com.javacodeset.dto.auth.AuthenticationResponseDto;
+import com.javacodeset.dto.auth.RegisterRequestDto;
+import com.javacodeset.dto.auth.RegisterResponseDto;
+import com.javacodeset.service.api.AuthenticationService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationRestController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public AuthenticationResponseDto login(@RequestBody AuthenticationRequestDto requestDto) {
+        return authenticationService.login(requestDto);
+    }
+
+    @PostMapping("/register")
+    public RegisterResponseDto register(@RequestBody RegisterRequestDto requestDto) {
+        return authenticationService.register(requestDto);
+    }
+}
