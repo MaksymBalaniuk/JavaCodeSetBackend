98688a60427d92e1e0b4685e52e70b696ddc2a45
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-05 19:35:25 +0300

    RestAuthenticationEntryPoint added

---
+++ src/main/java/com/javacodeset/config/RestAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.javacodeset.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import com.javacodeset.dto.error.ErrorResponseDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        String jsonString = objectMapper.writeValueAsString(new ErrorResponseDto(
+                HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage(), Instant.now()));
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getOutputStream().println(jsonString);
+        log.warn(authException.getMessage());
+    }
+}
