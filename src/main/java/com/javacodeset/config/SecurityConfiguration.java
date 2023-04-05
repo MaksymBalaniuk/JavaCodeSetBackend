9c85417d1259abdfcce12b0f96bde7e21627e34f
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-05 19:39:52 +0300

    Security configuration added

---
+++ src/main/java/com/javacodeset/config/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.javacodeset.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import com.javacodeset.security.jwt.JwtConfigurer;
+import com.javacodeset.security.jwt.JwtProvider;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final JwtProvider jwtProvider;
+    private final RestAuthenticationEntryPoint authenticationEntryPoint;
+    private final String[] authWhiteList;
+
+    public SecurityConfiguration(JwtProvider jwtProvider,
+                                 RestAuthenticationEntryPoint authenticationEntryPoint,
+                                 @Qualifier("authWhiteList") String[] authWhiteList) {
+        this.jwtProvider = jwtProvider;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authWhiteList = authWhiteList;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers(authWhiteList).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .cors()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+                .apply(new JwtConfigurer(jwtProvider, authenticationEntryPoint))
+                .and()
+                .build();
+    }
+}
