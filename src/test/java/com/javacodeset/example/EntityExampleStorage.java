197e45070aaca34c2739a492fb450d15fc0d8494
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-05-22 14:19:01 +0300

    CodeBlockRepositoryTest added

--- src/test/java/com/javacodeset/example/EntityExampleStorage.java
+++ src/test/java/com/javacodeset/example/EntityExampleStorage.java
@@ -1,6 +1,7 @@
 package com.javacodeset.example;
 
-import com.javacodeset.entity.AuthorityEntity;
+import com.javacodeset.entity.*;
+import com.javacodeset.enumeration.EstimateType;
 
 public final class EntityExampleStorage {
 
@@ -8,5 +9,35 @@ public final class EntityExampleStorage {
         AuthorityEntity authorityEntity = new AuthorityEntity();
         authorityEntity.setName("ROLE_SOME");
         return authorityEntity;
+    }
+
+    public static UserEntity getUserEntity() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername("username");
+        userEntity.setPassword("12345678");
+        userEntity.setEmail("username@gmail.com");
+        return userEntity;
+    }
+
+    public static CodeBlockEntity getCodeBlockEntity(UserEntity userEntity) {
+        CodeBlockEntity codeBlockEntity = new CodeBlockEntity();
+        codeBlockEntity.setUser(userEntity);
+        return codeBlockEntity;
+    }
+
+    public static EstimateEntity getEstimateEntity(UserEntity userEntity, CodeBlockEntity codeBlockEntity) {
+        EstimateEntity estimateEntity = new EstimateEntity();
+        estimateEntity.setUser(userEntity);
+        estimateEntity.setCodeBlock(codeBlockEntity);
+        estimateEntity.setType(EstimateType.LIKE);
+        return estimateEntity;
+    }
+
+    public static ShareEntity getShareEntity(UserEntity toUser, UserEntity fromUser, CodeBlockEntity codeBlock) {
+        ShareEntity shareEntity = new ShareEntity();
+        shareEntity.setToUser(toUser);
+        shareEntity.setFromUser(fromUser);
+        shareEntity.setCodeBlock(codeBlock);
+        return shareEntity;
     }
 }
---
+++ src/test/java/com/javacodeset/repository/CodeBlockRepositoryTest.java
@@ -0,0 +1,127 @@
+package com.javacodeset.repository;
+
+import com.javacodeset.entity.CodeBlockEntity;
+import com.javacodeset.entity.EstimateEntity;
+import com.javacodeset.entity.ShareEntity;
+import com.javacodeset.entity.UserEntity;
+import com.javacodeset.enumeration.EstimateType;
+import com.javacodeset.example.EntityExampleStorage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CodeBlockRepositoryTest {
+
+    @Autowired
+    private CodeBlockRepository codeBlockRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EstimateRepository estimateRepository;
+
+    @Autowired
+    private ShareRepository shareRepository;
+
+    @Test
+    public void findAllByUserId_codeBlockEntityExist_shodReturnNotEmptyCodeBlockEntityList() {
+        UserEntity userEntity = EntityExampleStorage.getUserEntity();
+        userRepository.save(userEntity);
+        CodeBlockEntity codeBlockEntity = EntityExampleStorage.getCodeBlockEntity(userEntity);
+        codeBlockRepository.save(codeBlockEntity);
+        List<CodeBlockEntity> expected = List.of(codeBlockEntity);
+
+        List<CodeBlockEntity> actual = codeBlockRepository.findAllByUserId(userEntity.getId());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllByUserId_codeBlockEntityNotExist_shodReturnEmptyCodeBlockEntityList() {
+        UserEntity userEntity = EntityExampleStorage.getUserEntity();
+        userRepository.save(userEntity);
+
+        List<CodeBlockEntity> actual = codeBlockRepository.findAllByUserId(userEntity.getId());
+
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void findAllCodeBlocksByUserIdAndEstimateType_codeBlockEntityExist_shodReturnNotEmptyCodeBlockEntityList() {
+        UserEntity userEntity = EntityExampleStorage.getUserEntity();
+        userRepository.save(userEntity);
+        CodeBlockEntity codeBlockEntity = EntityExampleStorage.getCodeBlockEntity(userEntity);
+        codeBlockRepository.save(codeBlockEntity);
+        EstimateEntity estimateEntity = EntityExampleStorage.getEstimateEntity(userEntity, codeBlockEntity);
+        estimateEntity.setType(EstimateType.LIKE);
+        estimateRepository.save(estimateEntity);
+        List<CodeBlockEntity> expected = List.of(codeBlockEntity);
+
+        List<CodeBlockEntity> actual =
+                codeBlockRepository.findAllCodeBlocksByUserIdAndEstimateType(userEntity.getId(), EstimateType.LIKE);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllCodeBlocksByUserIdAndEstimateType_codeBlockEntityNotExist_shodReturnEmptyCodeBlockEntityList() {
+        UserEntity userEntity = EntityExampleStorage.getUserEntity();
+        userRepository.save(userEntity);
+
+        List<CodeBlockEntity> actual =
+                codeBlockRepository.findAllCodeBlocksByUserIdAndEstimateType(userEntity.getId(), EstimateType.LIKE);
+
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void findAllCodeBlocksSharedFromUserIdToUserId_codeBlockEntityExist_shodReturnNotEmptyCodeBlockEntityList() {
+        UserEntity toUserEntity = EntityExampleStorage.getUserEntity();
+        toUserEntity.setUsername("1");
+        toUserEntity.setEmail("1@gmail.com");
+        userRepository.save(toUserEntity);
+        UserEntity fromUserEntity = EntityExampleStorage.getUserEntity();
+        fromUserEntity.setUsername("2");
+        fromUserEntity.setEmail("2@gmail.com");
+        userRepository.save(fromUserEntity);
+        CodeBlockEntity codeBlockEntity = EntityExampleStorage.getCodeBlockEntity(fromUserEntity);
+        codeBlockRepository.save(codeBlockEntity);
+        ShareEntity shareEntity = EntityExampleStorage.getShareEntity(toUserEntity, fromUserEntity, codeBlockEntity);
+        shareRepository.save(shareEntity);
+        List<CodeBlockEntity> expected = List.of(codeBlockEntity);
+
+        List<CodeBlockEntity> actual =
+                codeBlockRepository.findAllCodeBlocksSharedFromUserIdToUserId(
+                        fromUserEntity.getId(), toUserEntity.getId());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllCodeBlocksSharedFromUserIdToUserId_codeBlockEntityNotExist_shodReturnEmptyCodeBlockEntityList() {
+        UserEntity toUserEntity = EntityExampleStorage.getUserEntity();
+        toUserEntity.setUsername("1");
+        toUserEntity.setEmail("1@gmail.com");
+        userRepository.save(toUserEntity);
+        UserEntity fromUserEntity = EntityExampleStorage.getUserEntity();
+        fromUserEntity.setUsername("2");
+        fromUserEntity.setEmail("2@gmail.com");
+        userRepository.save(fromUserEntity);
+
+        List<CodeBlockEntity> actual =
+                codeBlockRepository.findAllCodeBlocksSharedFromUserIdToUserId(
+                        fromUserEntity.getId(), toUserEntity.getId());
+
+        assertTrue(actual.isEmpty());
+    }
+}
